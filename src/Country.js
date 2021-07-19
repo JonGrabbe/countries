74f78f330867f41afff36bcbb04b1c4182f61724
JonGrabbe <jgrabbe69@gmail.com>
2021-07-18 21:35:26 -0700

    added router

--- src/Country.js
+++ src/Country.js
@@ -1,8 +1,12 @@
+import {Link} from 'react-router-dom';
+
 export default function Country(props) {
     return(
+        <Link to={props.country.name}>
         <div className="country">
             <h2>Name: {props.country.name}</h2>
             <p>population: {props.country.population}</p>
         </div>
+        </Link>
     )
 }
--- src/Gallary.js
+++ src/Gallary.js
@@ -1,12 +1,26 @@
 import { CountriesContext } from "./CountriesContext"
 import Country from "./Country"
+import {
+    BrowserRouter,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 export default function Gallary(props) {
     return(
         <div className="gallery">
-            <CountriesContext.Consumer>
-                {(items) => items.map(item => <Country country={item}/>)}
-            </CountriesContext.Consumer>
+            <BrowserRouter>
+                <CountriesContext.Consumer>
+                    {(items) => items.map(item => <Country country={item}/>)}
+                </CountriesContext.Consumer>
+
+                <Switch>
+                    <Route path={}>
+                        
+                    </Route>
+                </Switch>
+            </BrowserRouter>
         </div>
     )
 }
