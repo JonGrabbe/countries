82136f5830a166060711d178ca6e5789f52820e6
JonGrabbe <jgrabbe69@gmail.com>
2021-07-26 08:32:04 -0700

    added methods to the body

--- src/App.js
+++ src/App.js
@@ -6,6 +6,39 @@ import './css/main.min.css';
 export default class App extends React.Component {
     constructor() {
         super()
+        this.state = {
+            countriesData: []
+        }
+    }
+
+    getCountryData(url) {
+        //fills the state with the array of objects the endpoint defined in the state
+        axios.get(url)
+            .then(res => {
+                this.setState({
+                    countriesData: res
+                })
+            })
+            .catch(err => {
+                console.log(err)
+                this.state.errorMsg = 'opps somthing went wrong'
+            })
+    }
+
+    setSearchURL() {
+        //sets the endpoint based on what option was chosen by the user 
+    }
+
+    setSearchTerm() {
+        //sets the search keyword
+    }
+
+    search() {
+
+    }
+
+    componentDidMount() {
+        this.getCountryData('https://restcountries.eu/rest/v2/all')
     }
 
     render() {
