b3161571daff9df8bd2ea60af2c8bd79aa02187b
JonGrabbe <jgrabbe69@gmail.com>
2021-07-27 05:26:57 -0700

    gallary and country card styles

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -2,12 +2,12 @@ export default function CountryCard(props) {
     return (
         <article className="country-card">
             <div className="flag-img-container">
-                {/* <img src={props.country.flag} /> */}
+                <img src={props.country.flag} />
             </div>
-            <h2>{props.country.name}</h2>
-            <h3>Population: {props.country.population}</h3>
-            <h3>Region: {props.country.region}</h3>
-            <h3>Capitial: {props.country.capital}</h3>
+            <h2 className="name">{props.country.name}</h2>
+            <h3 className="info"> <span className="text"> Population: </span> {props.country.population}</h3>
+            <h3 className="info"><span className="text"> Region: </span> {props.country.region}</h3>
+            <h3 className="info"><span className="text"> Capital: </span> {props.country.capital}</h3>
         </article>
     );
 }
