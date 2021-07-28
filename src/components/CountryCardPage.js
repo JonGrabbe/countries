1abf53a306a60b79b00a95ae88b4ea2d647574a6
JonGrabbe <jgrabbe69@gmail.com>
2021-07-28 10:49:46 -0700

    back to main page link placed on country card page

--- src/components/CountryCardPage.js
+++ src/components/CountryCardPage.js
@@ -1,13 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function CountryCardPage(props) {
-    const redirect = ()=> {
-        return <Redirect to="/" />
-    }
-    
     return (
         <div className="country-card-page">
-            <button className="back-button" >
+            <Link to="/" className="back-button">
                 Back
-            </button>
+            </Link>
             <h2>country card page</h2>
             <p>name: {props.country.name}</p>
         </div>
