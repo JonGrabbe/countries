d3b5f6afb9955fa80dbc349bb2a1903127d8792c
JonGrabbe <jgrabbe69@gmail.com>
2021-07-28 10:44:31 -0700

    redirect button

--- src/components/CountryCardPage.js
+++ src/components/CountryCardPage.js
@@ -1,6 +1,13 @@
 export default function CountryCardPage(props) {
+    const redirect = ()=> {
+        return <Redirect to="/" />
+    }
+    
     return (
         <div className="country-card-page">
+            <button className="back-button" >
+                Back
+            </button>
             <h2>country card page</h2>
             <p>name: {props.country.name}</p>
         </div>
