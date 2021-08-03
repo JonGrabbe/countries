5916aa9c49aab52ee2f5aedb747f84a9acd288ee
JonGrabbe <jgrabbe69@gmail.com>
2021-08-03 16:23:44 -0700

    added styles to the search bar

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,4 +1,6 @@
 import ThemeContext from "../context/theme-context";
+import darkSearch from '../images/search-dark.svg';
+import lightSearch from '../images/search-light.svg';
 
 export default function SearchInput(props) {
     return (
@@ -7,7 +9,7 @@ export default function SearchInput(props) {
                 themeValue => (
                     <div className="search-container">
                         <button onClick={props.handleClick} className="search-button button theme-element" >
-                            Search
+                            {<img src={themeValue.theme === 'light' ? darkSearch : lightSearch} />}
                         </button>
                         <input className="text-input theme-element" type="text" onChange={props.handleChange} placeholder="search for a country..." value={props.searchTerm} />
                     </div>
