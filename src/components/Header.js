87ba0f090d4045f0390d6e04079774153245cd1f
JonGrabbe <jgrabbe69@gmail.com>
2021-08-03 19:24:09 -0700

    worked on custom select component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,39 @@
-import MoonIcon from './MoonIcon';
-import SearchInput from './SearchInput';
-import Select from './Select';
-import {Route} from 'react-router-dom';
-import ThemeContext from '../context/theme-context';
+import MoonIcon from "./MoonIcon";
+import SearchInput from "./SearchInput";
+import Select from "./Select";
+import { Route } from "react-router-dom";
+import ThemeContext from "../context/theme-context";
+import CustomSelect from "./custom-select/Select";
 
 export default function Header(props) {
-    return (
-        <ThemeContext.Consumer>
-            {theme => (
-                <header className="header theme-background">
-                    <div className="inner-wrapper top theme-element">
-                        <h1 className="logo-text">Where in the world?</h1>
-                        <button className="theme-toggle" onClick={theme.toggleTheme} >
-                            {theme.theme !== 'light' ? <MoonIcon theme="light" /> : <MoonIcon theme="dark" /> }
-                            {theme.theme === 'light' ? 'dark mode' : 'light mode'}
-                        </button>
-                    </div>
-                    <Route path="/" exact>
-                        <div className="search-controls-container">
-                            <SearchInput handleChange={props.handleChangeText} handleClick={props.search} searchTerm={props.searchTerm} />
-                            <Select handleSelect={props.handleSelect} region={props.region} />
-                        </div>
-                    </Route>
-                </header>
-            )}
-            
-        </ThemeContext.Consumer>
-    );
+  return (
+    <ThemeContext.Consumer>
+      {(theme) => (
+        <header className="header theme-background">
+          <div className="inner-wrapper top theme-element">
+            <h1 className="logo-text">Where in the world?</h1>
+            <button className="theme-toggle" onClick={theme.toggleTheme}>
+              {theme.theme !== "light" ? (
+                <MoonIcon theme="light" />
+              ) : (
+                <MoonIcon theme="dark" />
+              )}
+              {theme.theme === "light" ? "dark mode" : "light mode"}
+            </button>
+          </div>
+          <Route path="/" exact>
+            <div className="search-controls-container">
+              <SearchInput
+                handleChange={props.handleChangeText}
+                handleClick={props.search}
+                searchTerm={props.searchTerm}
+              />
+              <Select handleSelect={props.handleSelect} region={props.region} />
+              <CustomSelect />
+            </div>
+          </Route>
+        </header>
+      )}
+    </ThemeContext.Consumer>
+  );
 }
--- src/components/custom-select/Select.js
+++ src/components/custom-select/Select.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react'
-import Select from 'react-select'
+import React from "react";
+import "./styles.css";
 
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
-]
+function SelectOptions(props) {
+  return (
+    <ul className="select-options">
+      {props.lis.map((item) => (
+        <li value={item}>{item}</li>
+      ))}
+    </ul>
+  );
+}
 
-const MyComponent = () => (
-  <Select options={options} />
-)
+const options = ["jan", "march", "april", "may"];
+
+export default function CustomSelect(props) {
+  return (
+    <div className="select">
+      <select className="select-hidden">
+        {props.options
+          ? props.options.map((item) => <option value={item}>{item}</option>)
+          : null}
+      </select>
+      <div className="select-styled"></div>
+      <SelectOptions lis={options} />
+    </div>
+  );
+}
