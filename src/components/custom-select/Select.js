09a21bac9bfe13081e929e706851274efcae81aa
JonGrabbe <jgrabbe69@gmail.com>
2021-08-04 17:26:36 -0700

    added styles to custom select component

--- src/components/custom-select/Select.js
+++ src/components/custom-select/Select.js
@@ -1,5 +1,6 @@
 import downArrowDarkTheme from '../../images/arrow-down-dark-theme.svg';
 import downArrowLightTheme from '../../images/arrow-down-light-theme.svg';
+import ThemeContext from '../../context/theme-context';
 
 function ListItem(props) {
   return (
@@ -19,18 +20,24 @@ function ListItem(props) {
 
 export default function CustomSelect(props) {
   return (
-    <div className="select-container theme-element">
-      <div className="select-title">
-        <span class="title">Filter by Region</span>
-        <img src={downArrowLightTheme} className="arrow" alt="" />
-      </div>
-      <ul className="select-list-container">
-        {
-          props.lis.map(item => (
-            <ListItem value={item} handleChange={props.handleChange} />
-          ))
-        }
-      </ul>
-    </div>
+    <ThemeContext.Consumer>
+      {
+        themeValue => (
+          <div className="select-container closed">
+            <div className="select-title theme-element">
+              <span class="title">Filter by Region</span>
+              <img src={themeValue.theme === 'dark' ? downArrowDarkTheme : downArrowLightTheme} className="arrow" alt="" />
+            </div>
+            <ul className="select-list-container theme-element">
+              {
+                props.lis.map(item => (
+                  <ListItem value={item} handleChange={props.handleChange} />
+                ))
+              }
+            </ul>
+          </div> 
+        )
+      }
+    </ThemeContext.Consumer>
   );
 }
