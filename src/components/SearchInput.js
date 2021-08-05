bc32342419e96735778dbec63e619819ba129531
JonGrabbe <jgrabbe69@gmail.com>
2021-08-05 10:03:34 -0700

    enter keypress triggers the search method

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -3,6 +3,14 @@ import darkSearch from '../images/search-dark.svg';
 import lightSearch from '../images/search-light.svg';
 
 export default function SearchInput(props) {
+    function handleKeyPress(e) {
+        // console.log(e.keyCode)
+        //13 is the enther key keycode
+        if(e.keyCode === 13) {
+            let value = e.currentTarget.value;
+            props.handleClick(value)
+        }
+    }
     return (
         <ThemeContext.Consumer>
             {
@@ -11,7 +19,13 @@ export default function SearchInput(props) {
                         <button onClick={props.handleClick} className="search-button button" >
                             {<img src={themeValue.theme === 'light' ? lightSearch : darkSearch} />}
                         </button>
-                        <input className="text-input" type="text" onChange={props.handleChange} placeholder="search for a country..." value={props.searchTerm} />
+                        <input 
+                            className="text-input" 
+                            type="text" 
+                            onChange={props.handleChange} 
+                            placeholder="search for a country..." value={props.searchTerm} 
+                            onKeyUp={handleKeyPress}
+                        />
                     </div>
                 )
             }
