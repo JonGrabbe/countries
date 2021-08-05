ecb44485de843b27d64025c02597beedbaaa0915
JonGrabbe <jgrabbe69@gmail.com>
2021-08-05 14:52:38 -0700

    added responsive styles

--- src/components/CountryCardPage.js
+++ src/components/CountryCardPage.js
@@ -18,21 +18,25 @@ function Content(props) {
             {
                 themeValue => (
                     <div className="country-card country-card-page">
-                    <Link to="/" className="back-button theme-element">
-                        <img src={themeValue.theme === 'light' ? leftArrowDark : leftArrowLight} className="back-arrow" alt="" />
-                        Back
-                    </Link>
+                    <div className="back-button-container">
+                        <Link to="/" className="back-button theme-element">
+                            <img src={themeValue.theme === 'light' ? leftArrowDark : leftArrowLight} className="back-arrow" alt="" />
+                            Back
+                        </Link>
+                    </div>
                     <div className="flag-img-container">
                         <img src={props.country.flag} alt="" />
                     </div>
                     <div className="country-info-container">
-                        <h2 className="name">{props.country.name}</h2>
-                        <InfoItem category="Native Name" value={props.country.nativeName} />
-                        <h3 className="info"> <span className="text"> Population: </span> {props.country.population}</h3>
-                        <h3 className="info"><span className="text"> Region: </span> {props.country.region}</h3>
-                        <InfoItem category="Sub Region" value={props.country.subregion} />
-                        <h3 className="info"><span className="text"> Capital: </span> {props.country.capital}</h3>
-                        <div className="info-spacer-group">
+                        <div className="info-group">
+                            <h2 className="name">{props.country.name}</h2>
+                            <InfoItem category="Native Name" value={props.country.nativeName} />
+                            <h3 className="info"> <span className="text"> Population: </span> {props.country.population}</h3>
+                            <h3 className="info"><span className="text"> Region: </span> {props.country.region}</h3>
+                            <InfoItem category="Sub Region" value={props.country.subregion} />
+                            <h3 className="info"><span className="text"> Capital: </span> {props.country.capital}</h3>
+                        </div>
+                        <div className="info-spacer-group info-group">
                             <InfoItem category="Top Level Domain" value={props.country.topLevelDomain[0]} />
                             <InfoItem category="Currencies" value="Euro" />
                             <InfoItem category="Languages" value={languages} />
